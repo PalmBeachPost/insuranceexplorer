3d03c8d7128c5de22959d595a63a737480bc19ff
kavyasukumar <kavya.sukumar@hotmail.com>
2014-11-18 18:09:33 -0500

    color coding - the ugly version

--- www/script/script.js
+++ www/script/script.js
@@ -39,19 +39,31 @@ $(document).ready( function () {
                     return data; 
                 } 
             }
-        ];
-        $.getJSON("./data/data.json", function(data) {
-            insuranceData = data;
-            drawTable();       
+    ];
+
+    d3.json("./data/data.json", function(data) {
+        insuranceData = data;
+        setUpScales();
+        drawTable();       
     });
     //$(window).resize(drawTable);
 });
+function setUpScales(){
+
+    colorScaleWeiss = d3.scale.linear()
+                        .domain(d3.extent(insuranceData.map( function(d){ return d.weissrank;})))
+                        .range(['#66ED22','#ED4729']);
+    colorScaleDemoTech = d3.scale.linear()
+                        .domain(d3.extent(insuranceData.map( function(d){ return d.demotechrank;})))
+                        .range(['#66ED22','#ED4729']);
+    colorScaleCompRatio = d3.scale.linear()
+                        .domain([0,6000])
+                        .range(['#ED4729','#66ED22',]);
+}
 function drawTable() {
 
     var table = $('#Table1');
     table.dataTable().fnClearTable();
-    
-    var coldef = Coldef;
 
     table.DataTable(
     {
@@ -61,16 +73,19 @@ function drawTable() {
         pageLength:80,
         oLanguage: {  sSearch: "Type company name to search: "},      
         data: insuranceData,
-        columns: coldef,
+        columns: Coldef,
         destroy:true,
         aoColumnDefs: [
          { "asSorting": [ "asc", "desc"], "aTargets": [ "_all"] }
         ],
         responsive: true,
         createdRow: function ( row, data, index ) {
-            if ( data['weissrank'] == 1 ) {
-                $('td', row).eq(1).css('color','green');
-            }
+            $('td', row).eq(1).css('background-color',colorScaleWeiss(data['weissrank']));
+            $('td', row).eq(2).css('background-color',colorScaleDemoTech(data['demotechrank']));
+            
+
+            var x = data['complaint_ratio'] == -1?6000:data['complaint_ratio'];
+            $('td', row).eq(3).css('background-color',colorScaleCompRatio(x));
         }
     });
 }
