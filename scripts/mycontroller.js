ed02a96d7729e2a1dcc848b8a0daf6119b51ce19
stucka <stucka@whitedoggies.com>
2017-08-21 17:16:54 -0400

    Tweaked mycontroller to use local files

--- scripts/mycontroller.js
+++ scripts/mycontroller.js
@@ -28,15 +28,15 @@ angular.module('InsuranceExplorerApp', ['ngRoute'])
 .config(['$routeProvider', function ($routeProvider) {
      $routeProvider
        .when('/', {
-            templateUrl: '//host.coxmediagroup.com/wpb/editorial/insuranceexplorer/TableView.html',
+            templateUrl: 'TableView.html',
             controller: 'TableCtrl'
         })
        .when('/detail/:companyId', {
            controller: 'DetailCtrl',
-           templateUrl: '//host.coxmediagroup.com/wpb/editorial/insuranceexplorer/DetailView.html'
+           templateUrl: 'DetailView.html'
         })
        .otherwise({
-            redirectTo: '//host.coxmediagroup.com/wpb/editorial/insuranceexplorer'
+            redirectTo: 'index.html'
        });
  }])
 .controller('TableCtrl', function ($scope, $filter, companyList, $http) {  
